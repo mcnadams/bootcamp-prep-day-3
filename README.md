08a9a989ebd1b565583a48499f6aca1275ade86a
mcnadams <mcneilbo@oregonstate.edu>
2019-01-16 20:42:24 -0800

    created a bunch of new files to play with JavaScript and eslint

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bootcamp-prep-day-3
 JavaScript FTW!
+
+(remember to put the script tag at the end of the body)
+
+Note: we set `.eslintrc` `no-console` to `off`
---
+++ main.js
@@ -0,0 +1,41 @@
+var x = 15;
+
+if(x > 12){
+    console.log(x); 
+    console.log('grooving with if');
+}
+else {
+    console.log('sorry, x too small');
+}
+
+// 1. initialize
+// 2. Condition
+// 3. Post Loop Action
+for(var i = 0; i < x; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+function sayHello() {
+    console.log('hi');
+}
+
+if(x % 2 === 0){
+    sayHello();
+}
+
+
+//eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+    console.log(input.value);
+    var y = input.value;
+
+    if(y % 2 === 0){
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
